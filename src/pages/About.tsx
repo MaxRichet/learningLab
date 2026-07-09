e3befbed09f10dfe388586060e0bc6b2b2a03433
klmr ✌🏼 <105491169+MaxRichet@users.noreply.github.com>
2026-07-09 10:19:09 +0200

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -23,7 +23,7 @@ export default function About() {
           <div className="grid md:grid-cols-2 gap-12 items-center">
             <div>
               <span className="badge mb-4">Our mission</span>
-              <h2 className="text-3xl font-bold text-gray-900 mb-4 mr-30">
+              <h2 className="text-3xl font-bold text-gray-900 mb-4 p-[40px]">
                 Radically reduce the cognitive overhead of shipping software.
               </h2>
               <p className="text-gray-500 leading-relaxed mb-4">
